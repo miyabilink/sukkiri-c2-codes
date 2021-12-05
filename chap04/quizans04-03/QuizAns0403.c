40ac525f228536df30865df7b40d4428866c4611
Tatsunori KAITO <kaito@miyabilink.jp>
2021-12-05 11:47:04 +0900

    紙面コード対応・誤植修正

--- chap04/quizans04-03/QuizAns0403.c
+++ chap04/quizans04-03/QuizAns0403.c
@@ -7,8 +7,9 @@ int main(void)
   if (tenki == true) {
     printf("洗濯をします\n");
     printf("散歩に行きます\n");
-  } else
+  } else {
     printf("映画を観ます\n");
     printf("映画の感想をブログに書きます\n");
+  }
   return 0;
 }
